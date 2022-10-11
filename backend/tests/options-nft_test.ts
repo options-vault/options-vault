12127da6ebf805a48652c8bcf902baf58253a9b4
Jack Tomlinson <jackiet333@protonmail.com>
2022-10-11 14:38:36 -0500

    redstone integration tests running, tidied toml

--- backend/tests/options-nft_test.ts
+++ backend/tests/options-nft_test.ts
@@ -1,10 +1,7 @@
 
-// @ts-ignore
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.0.2/index.ts';
-// @ts-ignore
-import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
-// @ts-ignore
-import axiod from "https://deno.land/x/axiod/mod.ts";
+import { Clarinet, Tx, Chain, Account, types, assertEquals, pricePackageToCV } from "./deps.ts";
+import type { PricePackage, Block } from "./deps.ts";
+import axiod from "https://deno.land/x/axiod@0.26.2/mod.ts";
 
 Clarinet.test({
     name: "Ensure that can start auction with submit price data",
@@ -13,55 +10,29 @@ Clarinet.test({
         const wallet_1 = accounts.get('wallet_1')?.address ?? ""
         const wallet_2 = accounts.get('wallet_2')?.address ?? ""
         const deployer = accounts.get('deployer')?.address ?? ""
-        let redstone_response;
+        let redstone_response = { timestamp: null, liteEvmSignature:"", value:0}
         await axiod.get("https://api.redstone.finance/prices?symbol=STX&provider=redstone").then((response) => {
             redstone_response = response.data[0]
         });
 
         console.log(redstone_response)
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-           Tx.contractCall("new-options-nft", "submit-price-data", [redstone_response.timestamp, redstone_response.value, redstone_response.liteEvmSignature], deployer)
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
-});
 
-Clarinet.test({
-    name: "Ensure that can start auction with submit price data",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const pricePackage: PricePackage = {
+			timestamp: redstone_response.timestamp ?? 0,
+			prices: [{ symbol: "STXUSD", value: redstone_response.value }]
+		}
 
-        const wallet_1 = accounts.get('wallet_1')?.address ?? ""
-        const wallet_2 = accounts.get('wallet_2')?.address ?? ""
-        const deployer = accounts.get('deployer')?.address ?? ""
-        let redstone_response;
-        await axiod.get("https://api.redstone.finance/prices?symbol=STX&provider=redstone").then((response) => {
-            redstone_response = response.data[0]
-        });
+		const packageCV = pricePackageToCV(pricePackage);
+		const signature = redstone_response.liteEvmSignature
 
-        console.log(redstone_response)
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-           Tx.contractCall("new-options-nft", "submit-price-data", [redstone_response.timestamp, redstone_response.value, redstone_response.liteEvmSignature], deployer)
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+		let block = chain.mineBlock([
+			Tx.contractCall("options-nft", "submit-price-data", [
+				packageCV.timestamp,
+				packageCV.prices,
+				signature
+			], wallet_1)
+		]);
 
         block = chain.mineBlock([
             /* 
