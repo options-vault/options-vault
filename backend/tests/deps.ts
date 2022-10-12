4093e6cfeba461ec440ba320c96a2888ab8e787f
Jack Tomlinson <jackiet333@protonmail.com>
2022-10-11 21:53:30 -0500

    tidy up

--- backend/tests/deps.ts
+++ backend/tests/deps.ts
@@ -36,7 +36,7 @@ export function pricePackageToCV(pricePackage: PricePackage) {
 }
 
  export function liteSignatureToStacksSignature(liteSignature:string) {
-	let bytes = hexToByte(liteSignature)
+	let bytes = hexToBytes(liteSignature)
 	bytes = bytes.slice(1, bytes.length)
 	if (bytes.length !== 65)
 		throw new Error(`Invalid liteSignature, expected 65 bytes got ${bytes.length}`);
@@ -52,20 +52,3 @@ function hexToBytes(hex: string) {
     return bytes;
 }
 
-function hexToByte(hex: string) {
-	const key = hex
-	let newBytes = []
-	let currentChar = 0
-	let currentByte = 0
-	for (let i=0; i<hex.length; i++) {   // Go over two 4-bit hex chars to convert into one 8-bit byte
-	  currentChar = key.indexOf(hex[i])
-	  if (i%2===0) { // First hex char
-		currentByte = (currentChar << 4) // Get 4-bits from first hex char
-	  }
-	  if (i%2===1) { // Second hex char
-		currentByte += (currentChar)     // Concat 4-bits from second hex char
-		newBytes.push(currentByte)       // Add byte
-	  }
-	}
-	return new Uint8Array(newBytes)
-  }
--- backend/tests/options-nft_test.ts
+++ backend/tests/options-nft_test.ts
@@ -8,15 +8,12 @@ Clarinet.test({
     async fn(chain: Chain, accounts: Map<string, Account>) {
 
         const wallet_1 = accounts.get('wallet_1')?.address ?? ""
-        const wallet_2 = accounts.get('wallet_2')?.address ?? ""
-        const deployer = accounts.get('deployer')?.address ?? ""
+        //const deployer = accounts.get('deployer')?.address ?? ""
+        
         let redstone_response = { timestamp: 0, liteEvmSignature:"", value:0}
         await axiod.get("https://api.redstone.finance/prices?symbol=STX&provider=redstone").then((response) => {
             redstone_response = response.data[0]
         });
-
-        console.log(redstone_response)
-		const signature = redstone_response.liteEvmSignature
 
 		let block = chain.mineBlock([
 			Tx.contractCall("options-nft", "submit-price-data", [
