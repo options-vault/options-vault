56c772e6fc23a76f6011657e4f209e5f508c8dbe
Andres Felipe Chaparro Carvajal <andreschc9@gmail.com>
2022-10-15 00:05:32 -0500

    Preset for end-current-cycle started

--- backend/tests/init.ts
+++ backend/tests/init.ts
@@ -125,3 +125,77 @@ export function initMint(
     ]);
     return block;
 }
+
+// For test end-current-clycle
+
+const lastTokenId = 5;
+const cycleExpiry = 65340234; // has to be changed
+const stxUsdRate = 231;
+const settlementExpiry = cycleExpiry;
+const srtike = 3000000;
+const optionsMintedAmount = 0;
+const trustedOraclePubkey = "0x035ca791fed34bf9e9d54c0ce4b9626e1382cf13daa46aa58b657389c24a751cc6";
+const untrustedOraclePubkey = "0x03cd2cfdbd2ad9332828a7a13ef62cb999e063421c708e863a7ffed71fb61c88c9";
+const pricePack = {
+  timestamp: 1647332581, // Tue Mar 15 2022 08:23:01 GMT+0000
+  prices: [{ symbol: "STXUSD", value: 2.5 }]
+};
+const Signature = "0x80517fa7ea136fa54522338145ebcad95f0be7c4d7c43c522fff0f97686d7ffa581d422619ef2c2718471c31f1af6084a03571f93e3e3bde346cedd2ced71f9100";
+
+type presetForEndCurrentCycle = {
+  chain: Chain,
+  accounts: Map<string,Account>,
+  lastTokenId?: Number,
+  currentCycleExpiry?: Number,
+  stxUsdRate?: Number,
+  strike?: Number,
+  optionsMintedAmount?: Number,
+  oraclePubKey?: String,
+  isOracleTrusted?: Boolean,
+  pricePackage?: PricePackage,
+  signature?: String
+}
+
+function setEnvironmentForEndCurrentCycle({
+  chain,
+  accounts,
+  lastTokenId,
+  currentCycleExpiry = cycleExpiry,
+  stxUsdRate,
+  strike,
+  optionsMintedAmount,
+  oraclePubKey = trustedOraclePubkey,
+  isOracleTrusted = true,
+  pricePackage = pricePack,
+  signature = Signature
+}: presetForEndCurrentCycle) {
+    const deployer = accounts.get('deployer')!.address;
+    const user_1 = accounts.get('wallet_1')!.address;
+    const packageCV = pricePackageToCV(pricePackage);
+    
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        'options-nft',
+        'set-trusted-oracle',
+        [ oraclePubKey, types.bool(isOracleTrusted) ],
+        deployer
+      ),
+      Tx.contractCall(
+        'options-nft',
+        'set-current-cycle-expiry',
+        [ types.uint(currentCycleExpiry) ],
+        deployer
+      ),
+      Tx.contractCall(
+        'options-nft',
+        'submit-price-data',
+        [ packageCV.timestamp, packageCV.prices, signature ],
+        deployer
+      )
+    ])
+}
+
+
+
+
+
